42b3004b323f4bfd8c20beb8b95acc942dca3a60
dAHli1909(J) <mrblead1909@gmail.com>
2023-11-08 22:47:49 -0600

    Changes to be committed: modified:   PrCafe/PrCafe.java

--- PrCafe/PrCafe.java
+++ PrCafe/PrCafe.java
@@ -4,48 +4,53 @@ package ETE2023_Java.PrCafe;
 *Autor:    Baca López Daniel Salvador 
 *Fecha:    06/11/2023
 *Descripcion: Cafetería 
-*
 */
 
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
-import java.text.DecimalFormat;
 
 public class PrCafe extends JFrame implements ActionListener{
-	private JLabel etiqueta; 
-	private JTextField campo;
-	private JButton boton; 
 
 	
-    
-    
-	private void crearGUI() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	
+
+	private JPanel crearPanel(Color color, int x, int y, int width, int height) {
+        JPanel panel = new JPanel();
+        panel.setBackground(color);
+        panel.setBounds(x, y, width, height);
+        return panel;
+    }
+	private void crearGUI(){
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
         Container ventana = getContentPane();
         ventana.setLayout(null);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setSize(800,350);
-        
-        
-    	
-    	setVisible(true);
+
+		Color café1 = new Color (0xCCAF7A);
+		Color café2 = new Color (0x99793D);
+		Color café3 = new Color (0x664914);
+		Color café4 = new Color (0x382400);
+		JPanel panelT = crearPanel(café4,0,0,1500,102);
+		JPanel panelST = crearPanel(café3, 0, 102, 1500, 100);
+		JPanel panelP = crearPanel(café1, 0, 201, 751, 700);
+		JPanel panelC = crearPanel(café2, 751, 201, 752, 700);
+		ventana.add(panelT);
+		ventana.add(panelST);
+		ventana.add(panelP);
+		ventana.add(panelC);
 	}
-	@Override
 	public void actionPerformed(ActionEvent event) {
-		// TODO Auto-generated method stub
-    	Object origen= event.getSource(); //permite trabajar con más de un botón
-    	
-        //Graphics papel = panel.getGraphics();
-    	
-		
+
+    	Object origen = event.getSource(); //permite trabajar con más de un botón
 	}
 	
     public static void main(String[] args) {
-        PrCafe demo = new PrCafe ();
-        demo.crearGUI();
-        demo.setTitle("Acomodar controles y panels");
-        demo.setVisible(true);
+        PrCafe marco = new PrCafe ();
+        marco.crearGUI();
+        marco.setTitle("Cafetería");
+		marco.setSize(1500,900);
+        marco.setVisible(true);
         
     }
 
