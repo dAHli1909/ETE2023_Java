1c3b4145f656a76cffccab2487d94ce78a27ff59
dAHli1909(J) <mrblead1909@gmail.com>
2023-11-24 02:31:55 -0600

    Changes to be committed:
    	modified:   PrCafe/PrCafe.java
        (añadí prácticamente todo; falta revisar si he de usar threads
         y la parte de billetes)
    	new file:   PrCafe/img/1.png
    	new file:   PrCafe/img/10.png
    	new file:   PrCafe/img/100.jpg
    	new file:   PrCafe/img/2.png
    	new file:   PrCafe/img/20.jpg
    	new file:   PrCafe/img/200.jpg
    	new file:   PrCafe/img/5.png
    	new file:   PrCafe/img/50.jpg
    	new file:   PrCafe/img/500.jpg

--- PrCafe/PrCafe.java
+++ PrCafe/PrCafe.java
@@ -20,48 +20,56 @@ public class PrCafe extends JFrame implements ActionListener{
 	Color café3 = new Color (0x664914);
 	Color café4 = new Color (0x382400);
 	Color negro = new Color (0x000000);
+	Color amarillo = new Color (0xEACC3C);
+	Color amarillo2 = new Color (0xE8C672);
+
+	//fonts;
+	Font tipoLetraG = new Font("Serif Plain",Font.BOLD,16);
+	Font tipoLetraT= new Font("Serif Bold Italic",Font.ITALIC,30);
 
 	//paneles
 	JPanel panelT = crearPanel(café4,0,0,1380,80,0);
 	JPanel panelST1 = crearPanel(café3, 0, 80, 690, 50, 0);
 	JPanel panelST2 = crearPanel(café3, 690, 80, 690, 50, 0);
 	JPanel panelP = crearPanel(café1, 0, 130, 690, 750, 0);
-	JPanel panelC = crearPanel(café2, 690, 130, 690, 400, 0);
+	JPanel panelC = crearPanel(café2, 690, 130, 690, 400, 1);
 	JPanel panelCS = crearPanel(café3, 690,530, 690, 165, 0);
 	JPanel opcionesPanel1, opcionesPanel2, opcionesPanel3, opcionesPanel4;
 
 	//interfaz gráfica; 
-	private JButton botonAme, botonExp, botonCap, botonMok, botonLat, botonSan, botonDon, botonPan, botonHel, pagar;
-	JButton botonAdelante = new JButton("Adelante");
-	JButton botonAtras = new JButton("Atrás");
-	JButton botonSelect = new JButton("Borrar");
+	private JButton botonAme, botonExp, botonCap, botonMok, botonLat, botonSan, botonDon, botonPan, botonHel, pagar, botonAdelante, botonAtras, botonBorrar;
 	private ButtonGroup bgOpcionesLeche, bgOpcionesExtra;
     private JRadioButton LecheEntera, LecheLight, LecheDeslactosada, CremaBatida, ChispasChocolate, CremaChispas, Ninguna;
 	JTextField pago;
 
 	//variables posteriormente acumulativas;
 	int contador = 0;
+	float PrecioI = 0;
 	String[] compraLista = new String[2]; 
 	String[] compraFrag;
+	char A = '0';
+	char B = '0';
+
+	JLabel muestraPrecio = new JLabel("Precio: " + String.valueOf(PrecioI));
 	
 
 	private JPanel crearPanel(Color color, int x, int y, int width, int height, int modificador) {
 		JPanel panel = new JPanel(new FlowLayout(FlowLayout.CENTER));
         if(modificador==1){
-			panel = new JPanel(new GridLayout(2,3));
+			panel = new JPanel(new GridLayout(9,1));
 		}
         panel.setBackground(color);
         panel.setBounds(x, y, width, height);
         return panel;
     }
-	private JPanel crearSubPanel(String archivo){
+	private JPanel crearSubPanel(String archivo, int w, int h, int wi, int hi){
 		JPanel panel = new JPanel(new FlowLayout(FlowLayout.CENTER, 20,12));
-		ImageIcon imgAme = new ImageIcon(new ImageIcon("./PrCafe/img/"+archivo).getImage().getScaledInstance(170, 120, Image.SCALE_DEFAULT));
+		ImageIcon imgAme = new ImageIcon(new ImageIcon("./PrCafe/img/"+archivo).getImage().getScaledInstance(wi,hi, Image.SCALE_DEFAULT));
 		JLabel AmeImageLabel = new JLabel();
 		AmeImageLabel.setIcon(imgAme);
 		panel.add(AmeImageLabel);
 		panel.setBackground(null);
-		panel.setPreferredSize(new Dimension(200,180));
+		panel.setPreferredSize(new Dimension(w,h));
 		return panel;
 	}
 	private void crearGUI(){
@@ -76,82 +84,91 @@ public class PrCafe extends JFrame implements ActionListener{
 		ventana.add(panelP);
 		ventana.add(panelC);
 		ventana.add(panelCS);
-		
+
 		JLabel lblTitulo = new JLabel("Cafetería jiji");
+		lblTitulo.setFont(tipoLetraT);
+		lblTitulo.setForeground(amarillo);
 		panelT.add(lblTitulo);
 		JLabel lblProductos = new JLabel("Productos");
+		lblProductos.setFont(tipoLetraG);
+		lblProductos.setForeground(amarillo2);
 		JLabel lblCompras = new JLabel("Compras");
+		lblCompras.setFont(tipoLetraG);
+		lblCompras.setForeground(amarillo2);
 		panelST1.add(lblProductos);
 		panelST2.add(lblCompras);
 		
-		botonAme = new JButton("Americano"); 
+		botonAme = new JButton("Americano $35"); 
 		botonAme.setPreferredSize(new Dimension(150,20));
-		JPanel panelPSubAme = crearSubPanel("imgAme.jpg");
+		JPanel panelPSubAme = crearSubPanel("imgAme.jpg", 200,180, 170,120);
 		panelPSubAme.add(botonAme);
 		panelP.add(panelPSubAme);
 		botonAme.addActionListener(this);
-		botonExp = new JButton("Exprés"); 
+		botonExp = new JButton("Exprés $60"); 
 		botonExp.setPreferredSize(new Dimension(150,20));
-		JPanel panelPSubExp = crearSubPanel("imgExp.jpg");
+		JPanel panelPSubExp = crearSubPanel("imgExp.jpg",200,180, 170,120);
 		panelPSubExp.add(botonExp);
 		panelP.add(panelPSubExp);
 		botonExp.addActionListener(this);
-		botonCap = new JButton("Capuchino"); 
+		botonCap = new JButton("Capuchino $70"); 
 		botonCap.setPreferredSize(new Dimension(150,20));
-		JPanel panelPSubCap = crearSubPanel("imgCap.jpg");
+		JPanel panelPSubCap = crearSubPanel("imgCap.jpg",200,180, 170,120);
 		panelPSubCap.add(botonCap);
 		panelP.add(panelPSubCap);
 		botonCap.addActionListener(this);
-		botonMok = new JButton("Moka"); 
+		botonMok = new JButton("Moka $90"); 
 		botonMok.setPreferredSize(new Dimension(150,20));
-		JPanel panelPSubMok = crearSubPanel("imgMok.jpg");
+		JPanel panelPSubMok = crearSubPanel("imgMok.jpg",200,180, 170,120);
 		panelPSubMok.add(botonMok);
 		panelP.add(panelPSubMok);
 		botonMok.addActionListener(this);
-		botonLat = new JButton("Latte"); 
+		botonLat = new JButton("Latte $90"); 
 		botonLat.setPreferredSize(new Dimension(150,20));
-		JPanel panelPSubLat = crearSubPanel("imgLat.jpg");
+		JPanel panelPSubLat = crearSubPanel("imgLat.jpg",200,180, 170,120);
 		panelPSubLat.add(botonLat);
 		panelP.add(panelPSubLat);
 		botonLat.addActionListener(this);
 
-		botonSan = new JButton("Sándwich"); 
+		botonSan = new JButton("Sándwich $100"); 
 		botonSan.setPreferredSize(new Dimension(150,20));
-		JPanel panelPSubSan = crearSubPanel("imgSan.jpg");
+		JPanel panelPSubSan = crearSubPanel("imgSan.jpg",200,180, 170,120);
 		panelPSubSan.add(botonSan);
 		panelP.add(panelPSubSan);
 		botonSan.addActionListener(this);
-		botonPan = new JButton("Panqué"); 
+		botonPan = new JButton("Panqué $40"); 
 		botonPan.setPreferredSize(new Dimension(150,20));
-		JPanel panelPSubPan = crearSubPanel("imgPan.jpg");
+		JPanel panelPSubPan = crearSubPanel("imgPan.jpg",200,180, 170,120);
 		panelPSubPan.add(botonPan);
 		panelP.add(panelPSubPan);
 		botonPan.addActionListener(this);
-		botonDon = new JButton("Dona"); 
+		botonDon = new JButton("Dona $30"); 
 		botonDon.setPreferredSize(new Dimension(150,20));
-		JPanel panelPSubDon = crearSubPanel("imgDon.jpg");
+		JPanel panelPSubDon = crearSubPanel("imgDon.jpg",200,180, 170,120);
 		panelPSubDon.add(botonDon);
 		panelP.add(panelPSubDon);
 		botonDon.addActionListener(this);
-		botonHel = new JButton("Helado"); 
+		botonHel = new JButton("Helado $120"); 
 		botonHel.setPreferredSize(new Dimension(150,20));
-		JPanel panelPSubHel = crearSubPanel("imgHel.jpg");
+		JPanel panelPSubHel = crearSubPanel("imgHel.jpg",200,180, 170,120);
 		panelPSubHel.add(botonHel);
 		panelP.add(panelPSubHel);
 		botonHel.addActionListener(this);
 
-		opcionesPanel1 = crearSubPanel("");
+		opcionesPanel1 = crearSubPanel("",200,180, 170,120);
 		panelCS.add(opcionesPanel1);
+		botonAdelante = new JButton("Adelante");
+		botonAtras = new JButton("Atrás");
+		botonBorrar = new JButton("Borrar");
 		JLabel lblopciones = new JLabel("Opciones:");
 		opcionesPanel1.add(lblopciones);
 		opcionesPanel1.add(botonAdelante);
 		opcionesPanel1.add(botonAtras);
-		opcionesPanel1.add(botonSelect);
+		opcionesPanel1.add(botonBorrar);
 		botonAdelante.addActionListener((this));
-		botonSelect.addActionListener((this));
+		botonBorrar.addActionListener((this));
 		botonAtras.addActionListener((this));
 
-		opcionesPanel2 = crearSubPanel("");
+		opcionesPanel2 = crearSubPanel("", 200,180, 170,120);
 		panelCS.add(opcionesPanel2);
 		JLabel lblLeche = new JLabel("Leche:");
 		opcionesPanel2.add(lblLeche);
@@ -165,11 +182,11 @@ public class PrCafe extends JFrame implements ActionListener{
 		opcionesPanel2.add(LecheEntera);
 		opcionesPanel2.add(LecheLight);
 		opcionesPanel2.add(LecheDeslactosada);
-		LecheEntera.addActionListener(this);
-		LecheDeslactosada.addActionListener(this);
-		LecheLight.addActionListener(this);
+		// LecheEntera.addActionListener((this));
+		// LecheLight.addActionListener((this));
+		// LecheDeslactosada.addActionListener((this));
 
-		opcionesPanel3 = crearSubPanel("");
+		opcionesPanel3 = crearSubPanel("", 200,180, 170,120);
 		panelCS.add(opcionesPanel3);
 		JLabel lblExtras = new JLabel("Extras");
 		opcionesPanel3.add(lblExtras);
@@ -187,15 +204,15 @@ public class PrCafe extends JFrame implements ActionListener{
 		opcionesPanel3.add(CremaChispas);
 		opcionesPanel3.add(Ninguna);
 		Ninguna.setSelected(true);
-		CremaBatida.addActionListener(this);
-		ChispasChocolate.addActionListener(this);
-		CremaChispas.addActionListener(this);
-		Ninguna.addActionListener(this);
+		CremaBatida.addActionListener((this));
+		ChispasChocolate.addActionListener((this));
+		CremaChispas.addActionListener((this));
+		Ninguna.addActionListener((this));
 
 		opcionesPanel2.setVisible(false);
 		opcionesPanel3.setVisible(false);
 		
-		opcionesPanel4 = crearSubPanel("");
+		opcionesPanel4 = crearSubPanel("", 200,180, 170,120);
 		panelCS.add(opcionesPanel4);
 		JLabel lblpago = new JLabel("PAGO");
 		pago = new JTextField();
@@ -205,6 +222,7 @@ public class PrCafe extends JFrame implements ActionListener{
 		opcionesPanel4.add(lblpago);
 		opcionesPanel4.add(pago);
 		opcionesPanel4.add(pagar);
+		opcionesPanel4.add(muestraPrecio);
 		pagar.addActionListener((this));
 		pago.addActionListener((this));
 		
@@ -216,91 +234,88 @@ public class PrCafe extends JFrame implements ActionListener{
 		Object origen = e.getSource(); 
 		String compra = "", identificador_secundario = ""; 
 		String[] compraSubFrag = new String[4];
-		char A = '0';
-		char B = '0';
+		
 		int identificador_inicial = 0;
-		CountDownLatch latch = new CountDownLatch(1);
-		Thread Select;
-
-		class Select implements Runnable {
-			private String nombre;
-			private CountDownLatch latch;
-			public Select(String nombre, CountDownLatch latch) {
-				this.nombre = nombre;
-				this.latch = latch;
-			}
-		}
-
 
 		if(origen!=pagar){
 			if(origen == botonAme){
 				identificador_inicial=10;
+				PrecioI+=35;
+				agregarNuevoPanel("imgAme.jpg","Americanoo",20,20);
 			}else if(origen == botonExp){
 				identificador_inicial=20;
+				PrecioI+=60;
+				agregarNuevoPanel("imgExp.jpg","Exprés",20,20);
 			}else if(origen == botonCap){
 				identificador_inicial=30;
+				PrecioI+=70;
+				agregarNuevoPanel("imgCap.jpg","Capuchino",20,20);
 			}else if(origen == botonMok){
 				identificador_inicial=40;
+				PrecioI+=90;
+				agregarNuevoPanel("imgMok.jpg","Moka",20,20);
 			}else if(origen == botonLat){
 				identificador_inicial=50;
+				PrecioI+=90;
+				agregarNuevoPanel("imgLat.jpg","Latte",20,20);
 			}else if(origen == botonSan){
 				compra = "01";
+				PrecioI+=100;
+				agregarNuevoPanel("imgSan.jpg","Sándwich",20,20);
 			}else if(origen == botonPan){
 				compra = "02";
+				PrecioI+=40;
+				agregarNuevoPanel("imgPan.jpg","Panqué",20,20);
 			}else if(origen == botonDon){
 				compra = "03";
+				PrecioI+=30;
+				agregarNuevoPanel("imgDon.jpg","Dona",20,20);
 			}else if(origen == botonHel){
 				compra = "04";
+				PrecioI+=120;
+				agregarNuevoPanel("imgHel.jpg","Helado",20,20);
 			}
+				
 
 			if(identificador_inicial>20){
 				LecheEntera.setSelected(true);
+				Ninguna.setSelected(true);
 				opcionesPanel2.setVisible(true);
 				opcionesPanel3.setVisible(true);
-				// Select = new Thread(new Select("Selección del tipo de leche", latch));
-				// Select.start();
-				// try{
-				// 	latch.await();
-				// }catch(InterruptedException IE){
-				// 	IE.printStackTrace();
-				// }
-				// botonSi.addEventListener("click", function() {
-				// 	// Coloca aquí el código que quieres que se ejecute cuando se hace clic en el botón "Sí"
-				// 	alert("¡Has hecho clic en Sí!");
-				// });
+				// Thread t=Thread.currentThread();
+				// t.interrupt();
 				
-				
-					if(LecheEntera.isSelected()){
+				if(LecheEntera.isSelected()){
 					A='1';
-					}else if(LecheDeslactosada.isSelected()){
-						A='2';
-					}else if(LecheLight.isSelected()){
-						A='3';
-					}else{
-						A='0';
-					}
-					if(CremaBatida.isSelected()){
-						B='1';
-					}else if(ChispasChocolate.isSelected()){
-						B='2';
-					}else if(CremaChispas.isSelected()){
-						B='3';
-					}else if(Ninguna.isSelected()){
-						B='0';
-					}
-				
-				
+				}else if(LecheDeslactosada.isSelected()){
+					A='2';
+				}else if(LecheLight.isSelected()){
+					A='3';
+				}else{
+					A='0';
+				}
+				if(CremaBatida.isSelected()){
+					B='1';
+				}else if(ChispasChocolate.isSelected()){
+					B='2';
+				}else if(CremaChispas.isSelected()){
+					B='3';
+				}else if(Ninguna.isSelected()){
+					B='0';
+				}
 			}
 			else{
 				opcionesPanel2.setVisible(false);
 				opcionesPanel3.setVisible(false);		
 			}
+			
+
 			identificador_secundario=""+A+B;
 			if(identificador_inicial>0){
 				compra = Integer.toString(identificador_inicial);
 			}
 			compra = compra + identificador_secundario;
-			//JOptionPane.showMessageDialog(null,"Unión de compra e identificador secundario: " + compra);
+			//JOptionPane.showMessageDialog(null,"Compra: " + compra);
 			contador++;
 			if(contador==1){
 				compraLista[0]=compra;
@@ -315,21 +330,22 @@ public class PrCafe extends JFrame implements ActionListener{
 		//Validación 
 		else if(origen == pagar){
 			if(isNumeric(pago.getText())){
+
 				//cálculos
 				float Precio = 0;
 				float CantidadPago = Float.parseFloat(pago.getText());
-				JOptionPane.showMessageDialog(null,"Cantidad a pagar:(float->string)" + String.valueOf(CantidadPago));
+				//JOptionPane.showMessageDialog(null,"Cantidad a pagar:(float->string)" + String.valueOf(CantidadPago));
 
 				int cantidadFragmentosG = (int) Math.ceil((double) compraLista[0].length() / 4);
-				JOptionPane.showMessageDialog(null,"CantidadFragmentosG:" + String.valueOf(cantidadFragmentosG));
+				//JOptionPane.showMessageDialog(null,"CantidadFragmentosG:" + String.valueOf(cantidadFragmentosG));
 
 				compraFrag=dividirCadena(compraLista[0], 4 );
-				JOptionPane.showMessageDialog(null,"comprafrag:" + Arrays.toString(compraFrag));
+				//JOptionPane.showMessageDialog(null,"comprafrag:" + Arrays.toString(compraFrag));
 
 				for(int i = 1; i <= cantidadFragmentosG; i++){
 					compraSubFrag=dividirCadena(compraFrag[i-1],1);
 					if(compraSubFrag[0].equals("1")){//americano
-						Precio+=35;
+						Precio+=35;	
 					}else if(compraSubFrag[0].equals("2")){//exprés
 						Precio+=60;
 					}else if(compraSubFrag[0].equals("3")){//capuchino
@@ -364,7 +380,8 @@ public class PrCafe extends JFrame implements ActionListener{
 					}else if(compraSubFrag[3].equals("0")){//nignuna
 						Precio+=0;
 					}
-					JOptionPane.showMessageDialog(null,"CicloA: " +  String.valueOf(i) + ", CicloB: " + " compraSubFrag:" + Arrays.toString(compraSubFrag) + " Precio: " + String.valueOf(Precio));
+					
+					//JOptionPane.showMessageDialog(null,"CicloA: " +  String.valueOf(i) + ", CicloB: " + " compraSubFrag:" + Arrays.toString(compraSubFrag) + " Precio: " + String.valueOf(Precio));
 				}
 				if(Precio <= CantidadPago){
 					int[] billetes = {0,0,0,0,0,0,0,0,0};
@@ -390,15 +407,67 @@ public class PrCafe extends JFrame implements ActionListener{
 							CantidadPago = Cambio(billetes, 8, 1, CantidadPago);
 						}
 					}
+					panelC.removeAll();
+					revalidate();
+        			repaint();
 					JOptionPane.showMessageDialog(null, "Billetes: 	" + Arrays.toString(billetes));
+					//JOptionPane.showMessageDialog(null, "Compra finalizada");					
+					for(int billete : billetes){
+						if(billete==billetes[0]){
+							muestraBilletes(billete, "500.jpg");
+						}else if(billete==billetes[1]){
+							muestraBilletes(billete, "200.jpg");
+						}else if(billete==billetes[2]){
+							muestraBilletes(billete, "100.jpg");
+						}else if(billete==billetes[3]){
+							muestraBilletes(billete, "50.jpg");
+						}else if(billete==billetes[4]){
+							muestraBilletes(billete, "20.jpg");
+						}else if(billete==billetes[5]){
+							muestraBilletes(billete, "10.png");
+						}else if(billete==billetes[6]){
+							muestraBilletes(billete, "5.png");
+						}else if(billete==billetes[7]){
+							muestraBilletes(billete, "2.png");
+						}else if(billete==billetes[8]){
+							muestraBilletes(billete, "1.png");
+						}
+						
+					}
 				}else{
 					JOptionPane.showMessageDialog(null,"Pago insuficiente");
 				}	
 			}
 		}
+		if(origen == botonBorrar){
+			//JOptionPane.showMessageDialog(null,"Borrar");
+			compraLista = new String[2];
+			contador = 0;
+			PrecioI=0;
+			muestraPrecio.setText("Precio: " + String.valueOf(PrecioI));
+			panelC.removeAll();
+			revalidate();
+			repaint();
+		}
 	}
+	private void muestraBilletes(int billete, String archivo){
+		while(billete>0){
+			agregarNuevoPanel(archivo, "",50,40);
+			billete--;
+		}
+	}
+	private void agregarNuevoPanel(String archivo, String muestraProducto, int wi, int hi) {
+        JPanel panelNuevo = crearSubPanel(archivo, 690,35,wi,hi);
+        panelNuevo.setBackground(null);
+		JLabel muestraProduct = new JLabel(muestraProducto);
+		panelNuevo.add(muestraProduct);
+		panelC.add(panelNuevo);
+		muestraPrecio.setText("Precio: " + String.valueOf(PrecioI));
+        revalidate();
+        repaint();
+    }
 	static float Cambio(int[] arrayP, int i, int n, float P){
-		arrayP[i] = (int) Math.floor((double) P / n);
+		arrayP[i] = (int) Math.floor(P / n);
 		P-=arrayP[i]*n;
 		return P;
 	}
